38225c71df1ccdb26213b1709c4c5b23c56a477e
Ekiserrepé <ekiserrepe@gmail.com>
2024-05-06 18:16:10 +0200

    Updated v.0.1

--- background.js
+++ background.js
@@ -1,24 +1,52 @@
-chrome.runtime.onInstalled.addListener(() => {
-  function updateBadge(price) {
-    chrome.action.setBadgeText({ text: xahauPrice.toString() });
-  }
+let originalBadgeColor = '';
 
-  function fetchXahauPrice() {
+function updateBadge(price) {
+    chrome.action.setBadgeText({ text: price.toString() });
+
+    if (originalBadgeColor === '') {
+        chrome.action.getBadgeBackgroundColor({}, result => {
+            originalBadgeColor = result;
+        });
+    }
+
+    chrome.action.setBadgeBackgroundColor({ color: [255, 215, 0, 255] });
+
+    
+    setTimeout(() => {
+        chrome.action.setBadgeBackgroundColor({ color: originalBadgeColor });
+    }, 1000); // (1 second)
+}
+
+function fetchxahauPrice() {
     fetch('https://api.coingecko.com/api/v3/simple/price?ids=xahau&vs_currencies=usd')
-      .then(response => response.json())
-      .then(data => {
-         const xahauPrice = data.xahau.usd;
-         const formattedPrice = `${xahauPrice.toFixed(3)}`;
-        chrome.action.setBadgeText({ text: `${formattedPrice}` });
-      })
-      .catch(error => {
-        console.error('Error fetching Xahau price:', error);
-        updateBadge('Error');
-      });
-  }
+        .then(response => response.json())
+        .then(data => {
+            const xahauPrice = parseFloat(data.xahau.usd);
+            const formattedPrice = `${xahauPrice.toFixed(3)}`;
+            updateBadge(formattedPrice);
+        })
+        .catch(error => {
+            console.error('Error fetching XRP price:', error);
+            updateBadge('Error');
+        });
+}
 
-  fetchXahauPrice(); 
-
-  
-  setInterval(fetchXahauPrice, 300000); 
+chrome.runtime.onInstalled.addListener(() => {
+    fetchxahauPrice();
 });
+
+chrome.runtime.onStartup.addListener(() => {
+    fetchxahauPrice();
+});
+
+chrome.action.onClicked.addListener(() => {
+    fetchxahauPrice();
+});
+
+chrome.idle.onStateChanged.addListener(state => {
+    if (state === 'active') {
+        fetchxahauPrice();
+    }
+});
+
+setInterval(fetchxahauPrice, 300000); //5 Minutes
